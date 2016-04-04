adbe38851d8c7e2e802342022bb28d65db4530cd
Dane Carmichael <carmichaeldane@gmail.com>
2016-04-04 17:28:20 -0500

    Full crud for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,45 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.page(params[:page])
+    @tasks = Task.order(:position)
   end
 
   def show
+    @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.create(body: params[:task][:body])
+    task.position = task.id
+    @task = task
+    task.save!
+    redirect_to task_path(@task.id)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to @task
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to :back
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:body)
+    end
 end
