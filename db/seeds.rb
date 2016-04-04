aae256ee83dc53049638c0ee4bc2ee0a3b08fe23
Dane Carmichael <carmichaeldane@gmail.com>
2016-04-04 13:51:22 -0500

    Seeded the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Task.create(body: "Make a to do list", position: 1)
